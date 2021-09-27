cc4a55639fd4b97782d11a0ad28b3157565d8d4a
nchavan007 <50837588+nchavan007@users.noreply.github.com>
2021-09-26 23:01:27 -0700

    Update db_fetch.py

--- db_fetch.py
+++ db_fetch.py
@@ -6,7 +6,6 @@ def SelSqlFun():
     """" This function will fetch the information from table for certain critiria() """
     with open(r"mysql_querry", "r") as file:
         data = json.load(file)
-
     db = pymysql.connect(host=data["DB_HOST"], user=data["DB_USER"], password=data["DB_PASSWD"], database=data["DB_NAME"], port=data["DB_PORT"])
     cursor = db.cursor()
     sql = f'SELECT {data["DB_FIELD"]} FROM {data["TABLE_NAME"]} WHERE app_code IN ({data["APP_CODE"]});'
@@ -16,18 +15,48 @@ def SelSqlFun():
        # Fetch all the rows in a list of lists.
        results = cursor.fetchall()
        for row in results:
-           if ( row[7] = "PROD" OR row[7] = "DR" ):
+           if ( row[6] == 'PROD' or row[6] == 'DR'):
                env="prod"
-               if( row[4] = "Windows" OR row[4] = "Linux"):
+               if( row[3] == "Windows" or row[3] == "Linux"):
                    infra="OS"
                    template1 = load_workbook(filename=f"template1_{env}_{infra}.xlsx")
                    main_sheet = template1.active
                    current_row = main_sheet.max_row
                    next_row = current_row + 1
-                   main_sheet[f'B{next_row}'] = row[server_name]
-                   main_sheet[f'F{next_row}'] = row[OS_version]
-    except:
-       print ("Error: unable to fetch data")
+                   main_sheet[f'B{next_row}'] = row[5]
+                   main_sheet[f'F{next_row}'] = row[4]
+                   template1.save(f"template1_{env}_{infra}.xlsx")
+               elif( row[3] == "Oracle" or row[3] == "SQLServer"):
+                   infra="DB"
+                   template1 = load_workbook(filename=f"template1_{env}_{infra}.xlsx")
+                   main_sheet = template1.active
+                   current_row = main_sheet.max_row
+                   next_row = current_row + 1
+                   main_sheet[f'B{next_row}'] = row[5]
+                   main_sheet[f'G{next_row}'] = row[4]
+                   template1.save(f"template1_{env}_{infra}.xlsx")
+           elif ( row[6] == 'DEV' or row[6] == 'QA'):
+               env="nonprod"
+               if( row[3] == "Windows" or row[3] == "Linux"):
+                   infra="OS"
+                   template1 = load_workbook(filename=f"template1_{env}_{infra}.xlsx")
+                   main_sheet = template1.active
+                   current_row = main_sheet.max_row
+                   next_row = current_row + 1
+                   main_sheet[f'B{next_row}'] = row[5]
+                   main_sheet[f'D{next_row}'] = row[4]
+                   template1.save(f"template1_{env}_{infra}.xlsx")
+               elif( row[3] == "Oracle" or row[3] == "SQLServer"):
+                   infra="DB"
+                   template1 = load_workbook(filename=f"template1_{env}_{infra}.xlsx")
+                   main_sheet = template1.active
+                   current_row = main_sheet.max_row
+                   next_row = current_row + 1
+                   main_sheet[f'B{next_row}'] = row[5]
+                   main_sheet[f'C{next_row}'] = row[4]
+                   template1.save(f"template1_{env}_{infra}.xlsx")
+    except Exception as e:
+       print (f"Error: unable to fetch data with error {e}")
 
     # disconnect from server
     db.close()
